0bce1cbf4b049c1ac6806207a04c019cb3f12a97
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-09-13 00:52:15 +0300

    Add method RegisterCreditCard

--- ShopLogic/Models/Customer.cs
+++ ShopLogic/Models/Customer.cs
@@ -8,7 +8,7 @@ namespace ShopLogic.Models
         public string Login { get; set; }
         public string Password { get; set; }
         public DateTime DateOfRegistration { get; init; }
-        public CreditCard CreditCard { get; set; }
+        public CreditCard? CreditCard { get; set; }
 
         public Customer(string login, string password, DateTime dateOfRegistration, string first_name, string last_name, DateTime birthDate, string emailAdress)
             : base(first_name, last_name, birthDate, emailAdress)
@@ -16,6 +16,11 @@ namespace ShopLogic.Models
             Login = login;
             Password = password;
             DateOfRegistration = dateOfRegistration;
+        }
+        
+        public void RegisterCreditCard(int Number, decimal Money)
+        {
+            CreditCard = new CreditCard(Number, Money);
         }
 
         public void Buy()
