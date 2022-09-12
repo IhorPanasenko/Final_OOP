2612d35b3ec5a8a9cb2813ab2d95e69237a8bd3b
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-09-13 00:21:53 +0300

    Add methods

--- ShopLogic/Interfaces/ICRUD.cs
+++ ShopLogic/Interfaces/ICRUD.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace ShopLogic.Interfaces
 {
     internal interface ICRUD
     {
+        public void Update();
+        public void Delete();
+        public void Read();
     }
 }
