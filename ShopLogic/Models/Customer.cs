7ee8885913387b07568886133cb2d7d8cf0737c3
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-09-13 00:50:22 +0300

    Add CreditCard to Customer

--- ShopLogic/Models/Customer.cs
+++ ShopLogic/Models/Customer.cs
@@ -1,16 +1,16 @@
 ﻿
 using System;
 
-namespace ShopLogic
+namespace ShopLogic.Models
 {
     internal class Customer : Person, IBuyable
     {
         public string Login { get; set; }
         public string Password { get; set; }
         public DateTime DateOfRegistration { get; init; }
+        public CreditCard CreditCard { get; set; }
 
-
-        public  Customer(string login, string password, DateTime dateOfRegistration, string first_name, string last_name, DateTime birthDate, string emailAdress)
+        public Customer(string login, string password, DateTime dateOfRegistration, string first_name, string last_name, DateTime birthDate, string emailAdress)
             : base(first_name, last_name, birthDate, emailAdress)
         {
             Login = login;
