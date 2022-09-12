d5619b9cf56c719fbbf726dc4c2411a04bd2b915
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-09-13 01:14:46 +0300

    Add SingleProduct realization

--- ShopLogic/Models/SingleOrder.cs
+++ ShopLogic/Models/SingleOrder.cs
@@ -4,5 +4,31 @@ namespace ShopLogic.Models
 {
     internal class SingleOrder
     {
+        private int quantity;
+        internal Product SingleProduct;
+        internal int Quantity { get { return quantity; } set { quantity = value>=1?value:1;} }
+
+        public SingleOrder(Product product, int quantity)
+        {
+            
+            SingleProduct = product;
+            this.quantity = quantity;
+        }
+
+        public void DecreaseQuantity()
+        {
+            if (quantity > 1)
+            {
+                quantity -= 1;
+            }
+            else
+            {
+                Console.WriteLine($"In your order quantity of {SingleProduct.Name} is already 1\nYou cant order less");
+            }
+        }
+        public void IncreaseQuantity()
+        {
+            quantity += 1;
+        }
     }
 }
