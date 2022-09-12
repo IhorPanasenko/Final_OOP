25efc6f52271cbe5152eae9052085a48d06d7ecd
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-09-13 02:15:18 +0300

    Add new property for order

--- ShopLogic/Models/Order.cs
+++ ShopLogic/Models/Order.cs
@@ -8,13 +8,15 @@ namespace ShopLogic.Models
         public DateTime DateOfOrder { get; init; }
         public Basket Basket { get; init; }
         public  Customer Customer { get; init; }
+        public decimal TotalPrice { get; init; }
 
 
-        public Order(DateTime dateOfOrder, Basket basket, Customer customer)
+        public Order(DateTime dateOfOrder, Basket basket, Customer customer, int totalPrice)
         {
             DateOfOrder = dateOfOrder;
             Basket = basket;
             Customer = customer;
+            TotalPrice = totalPrice;
         }
     }
 }
