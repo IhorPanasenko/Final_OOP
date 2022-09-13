7ec83913ccaa9c42d484a319814edbca4e13922a
Roman Riabenko <beatle.gaze@gmail.com>
2022-09-13 14:57:37 +0300

    Validation for Person

--- ShopLogic/Models/Person.cs
+++ ShopLogic/Models/Person.cs
@@ -5,14 +5,18 @@ namespace ShopLogic.Models
 {
     internal abstract class Person
     {
-        public string First_name { get; set; }
-        public string Last_name { get; set; }
+        public string? First_name { get; set; }
+        public string ?Last_name { get; set; }
         public DateTime BirthDate { get; init; }
-        public string EmailAdress { get; set; }
+        public string? EmailAdress { get; set; }
 
 
         public Person(string first_name, string last_name, DateTime birthDate, string emailAdress)
         { 
+            if (String.IsNullOrEmpty(first_name) || String.IsNullOrEmpty(last_name) || String.IsNullOrEmpty(emailAdress))
+            {
+                throw new ArgumentException("Fullname and email adress cant be null or empty");
+            }
             First_name = first_name;
             Last_name = last_name;
             BirthDate = birthDate;
