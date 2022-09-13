23e2d84a3fe48b0eed48c1d060e0a0f86a5090e4
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-09-13 19:47:12 +0300

    add new Admin function

--- ShopLogic/Interfaces/IAdministration.cs
+++ ShopLogic/Interfaces/IAdministration.cs
@@ -6,11 +6,18 @@ namespace ShopLogic.Interfaces
     internal interface IAdministration
     {
         public Category CrateCategory(string Name);
+
         public void DeleteCategory(Category category);
+
         public void UpdateCategory(Category category, string newName);
+
         public Product CreateProduct(string name, string description, int totalAmount, int price);
+
         public void DeleteProduct(Product product);
+
         public void UpdateProduct(Product product, string? name=null, string? description=null, int totalAmount=-1, int price = -1);
+
+        public void putProductInShop(E_shop shop, Warehouse warehouse, Category category, Product product);
 
     }
 }
